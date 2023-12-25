6c9a76c8286a3ca1a1740c345ad71020c025d18a
Anthony <amichaelson@protonmail.com>
2023-12-25 13:05:34 -0800

    added support for bun runtime env

--- nodejs/up.sh
+++ nodejs/up.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+export JS_IMAGE=${1:-"node:21.5.0-slim"}
+export JS_START_CMD=${2:-"node /app/server.js"}
+
 export NUMBER_OF_PRIMES=${NUMBER_OF_PRIMES:-1500}
 export STREAMING_SERVER_PORT=${STREAMING_SERVER_PORT:-3030}
 export STRING_CONCAT_SIZE=${STRING_CONCAT_SIZE:-10000}
-export API_CALL_DELAY_MS=${API_CALL_DELAY_MS:-2000}
+export API_CALL_DELAY_MS=${API_CALL_DELAY_MS:-1000}
 export STREAM_SIZE=${STREAM_SIZE:-1000000}
 
 if ! command -v docker-compose &>/dev/null; then
