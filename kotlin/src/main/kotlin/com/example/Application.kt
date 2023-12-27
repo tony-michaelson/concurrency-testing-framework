25d541ca451c20a4cf7aab80d728228d5dd2c2c7
Anthony <amichaelson@protonmail.com>
2023-12-27 15:39:48 -0800

    env var defined worker and call group

--- kotlin/src/main/kotlin/com/example/Application.kt
+++ kotlin/src/main/kotlin/com/example/Application.kt
@@ -7,7 +7,15 @@ import io.ktor.server.netty.*
 
 fun main() {
     val PORT = System.getenv("PORT") ?: "3000"
-    embeddedServer(Netty, port = PORT.toInt(), host = "0.0.0.0", module = Application::module)
+    val workerGroupSizeEnv = System.getenv("WORKER_GROUP_SIZE")
+    val callGroupSizeEnv = System.getenv("CALL_GROUP_SIZE")
+    val workerGroupSize = workerGroupSizeEnv?.toIntOrNull() ?: 16
+    val callGroupSize = callGroupSizeEnv?.toIntOrNull() ?: 16
+
+    embeddedServer(Netty, port = PORT.toInt(), host = "0.0.0.0", module = Application::module, configure = {
+        workerGroupSize
+        callGroupSize
+    })
         .start(wait = true)
 }
 
