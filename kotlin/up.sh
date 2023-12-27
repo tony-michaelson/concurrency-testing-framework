875e81f9bf3a08990b2a374c91cc84bcb2372507
Anthony <amichaelson@protonmail.com>
2023-12-27 15:42:28 -0800

    added settings for worker and call group size with setings displayed at the top

--- kotlin/up.sh
+++ kotlin/up.sh
@@ -5,6 +5,16 @@ export STREAMING_SERVER_PORT=${STREAMING_SERVER_PORT:-3030}
 export STRING_CONCAT_SIZE=${STRING_CONCAT_SIZE:-10000}
 export API_CALL_DELAY_MS=${API_CALL_DELAY_MS:-1000}
 export STREAM_SIZE=${STREAM_SIZE:-1000000}
+export WORKER_GROUP_SIZE=${WORKER_GROUP_SIZE:-16}
+export CALL_GROUP_SIZE=${CALL_GROUP_SIZE:-16}
+
+echo "Worker group size: $WORKER_GROUP_SIZE"
+echo "Call group size: $CALL_GROUP_SIZE"
+echo "Number of primes: $NUMBER_OF_PRIMES"
+echo "Streaming server port: $STREAMING_SERVER_PORT"
+echo "String concat size: $STRING_CONCAT_SIZE"
+echo "API call delay: $API_CALL_DELAY_MS"
+echo "Stream size: $STREAM_SIZE"
 
 if ! command -v docker-compose &>/dev/null; then
   echo "Docker Compose is not installed. Please install it and try again."
@@ -17,3 +27,4 @@ if [ ! -f "docker-compose.yml" ]; then
 fi
 
 docker-compose up
+docker-compose down
